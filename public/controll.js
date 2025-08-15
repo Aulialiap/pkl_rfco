1b9311afb67de8cb36e9b69026c1ce68ac738ba1
Aulialiap <liapuutri2007@gmail.com>
2025-08-15 16:05:41 +0700

    done add and delete system

--- public/controll.js
+++ public/controll.js
@@ -73,6 +73,11 @@ function createSystemCard(key, data) {
             <div class="system-title">
                 <input type="text" value="${data.name}" onchange="updateSystemName('${key}', this.value)">
             </div>
+            <button class="delete-system-btn" onclick="deleteSystem('${key}')">
+                <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-trash3-fill" viewBox="0 0 16 16">
+                    <path d="M11 1.5v1h3.5a.5.5 0 0 1 0 1h-.538l-.853 10.66A2 2 0 0 1 11.115 16h-6.23a2 2 0 0 1-1.994-1.84L2.038 3.5H1.5a.5.5 0 0 1 0-1H5v-1A1.5 1.5 0 0 1 6.5 0h3A1.5 1.5 0 0 1 11 1.5m-5 0v1h4v-1a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 0-.5.5M4.5 5.029l.5 8.5a.5.5 0 1 0 .998-.06l-.5-8.5a.5.5 0 1 0-.998.06m6.53-.528a.5.5 0 0 0-.528.47l-.5 8.5a.5.5 0 0 0 .998.058l.5-8.5a.5.5 0 0 0-.47-.528M8 4.5a.5.5 0 0 0-.5.5v8.5a.5.5 0 0 0 1 0V5a.5.5 0 0 0-.5-.5"/>
+                </svg>
+            </button>
         </div>
         <div class="port-info">
             <div class="port-row">
@@ -180,7 +185,7 @@ async function connectESP(name, ip) {
             } else if (data.currentPath === 2) {
                 systemsData[key].currentPath = 'backup';
             } //else if (data.currentPath === 0) {
-                //systemsData[key].currentPath = 'unknown';
+            //systemsData[key].currentPath = 'unknown';
             //}
             updateCurrentPathDisplay(key);
         }
@@ -348,6 +353,48 @@ function updateSystemPath(k, pathKey, v) {
     systemsData[k][pathKey] = v.trim();
     updateCurrentPathDisplay(k);
 }
+
+function addNewSystemInline() {
+    const count = Object.keys(systemsData).length;
+    const newIndex = count + 1;
+    const newKey = newIndex;
+
+    const newSystemData = {
+        name: `Sistem ${newIndex}`,      // <-- otomatis
+        hostname1: '',
+        port1: '',
+        hostname2: '',
+        port2: '',
+        jalur_normal: '',
+        jalur_backup: '',
+        currentPath: 'Unknown',
+        index: newIndex
+    };
+
+    systemsData[newKey] = newSystemData;
+    locationData.systems[newKey] = newSystemData;
+
+    const container = document.getElementById('systems-container');
+    const newCard = createSystemCard(newKey, newSystemData);
+    container.appendChild(newCard);
+
+    showNotification(`Sistem ${newIndex} ditambahkan.`, 'info');
+}
+
+function deleteSystem(key) {
+    // Konfirmasi dulu ke user
+    if (!confirm(`Yakin ingin menghapus ${systemsData[key].name}?`)) return;
+
+    // Hapus dari object
+    delete systemsData[key];
+    delete locationData.systems[key];
+
+    // Regenerate ulang semua card
+    generateSystemCards();
+
+    showNotification(`Sistem berhasil dihapus.`, 'success');
+}
+
 
 // ⏺ Simpan ke server!
 function saveAllConfigurations() {
